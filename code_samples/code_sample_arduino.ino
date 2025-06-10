6477f782e9d476eae3a7d386a40a520d487f5093
Edu Radu-Ștefan <83552731+raduedu@users.noreply.github.com>
2025-06-10 20:00:18 +0300

    Update code_sample_arduino.ino language
    Changed the language of the code (including variables and comments) from Romanian to English

--- code_samples/code_sample_arduino.ino
+++ code_samples/code_sample_arduino.ino
@@ -10,122 +10,123 @@
 ModbusRTUSlave slave(Serial);
 uint16_t registers[8];
  
-// variabile de intrare partea 2
+// input variables part 2
 Servo servo_11;
  
-// variabile de intrare partea 3
+// input variables part 3
 Servo servo_10;
  
-// variabile de intrare si functii partea 4
-void alarma_sol() // functie alarma sunet
+// input variables and functions part 4
+void sound_alarm() // sound alarm function
 {
-  digitalWrite(13,HIGH);
+  digitalWrite(13, HIGH);
   tone(3, 532);
   delay(125);
   delay(200);
-  digitalWrite(13,LOW);
+  digitalWrite(13, LOW);
   noTone(3);
   delay(200);
 }
  
-// variabile de intrare si functii partea 5
-void ventilator (int speedpin, int speed)  //functie functionare ventilator
+// input variables and functions part 5
+void control_fan(int speedPin, int speed)  // fan operation function
 {
   if (speed <= 0)
   {
-    analogWrite(speedpin, 0);
+    analogWrite(speedPin, 0);
   }
   else if (speed > 255)
   {
-    analogWrite(speedpin, 255);
+    analogWrite(speedPin, 255);
   }
   else
   {
-    analogWrite(speedpin, speed);
+    analogWrite(speedPin, speed);
   }
 }
  
-// variabile de intrare partea 6
+// input variables part 6
 DHT dht12(12, 11);
  
-// variabile de intrare si functii partea 7
-int rosu;
-int verde;
-int timp;
-String parola;
-int usa;
-boolean acces_usa;
-LiquidCrystal_I2C mylcd(0x27,16,2);
+// input variables and functions part 7
+int red;
+int green;
+int timeCounter;
+String password;
+int door;
+boolean doorAccess;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo servo_9;
-void cheie()
+
+void play_key_tone()
 {
-  tone(3,349);
+  tone(3, 349);
   delay(125);
   delay(100);
   noTone(3);
   delay(100);
 }
-void confirmare_parola()
+
+void confirm_password()
 {
-  if (0 == verde && 0 != rosu)
+  if (green == 0 && red != 0)
   {
     delay(100);
-    rosu = digitalRead(5);
-    if (0 == verde && 0 != rosu)
+    red = digitalRead(5);
+    if (green == 0 && red != 0)
     {
-      if (false == acces_usa)
+      if (!doorAccess)
       {
-        if (parola == "--")
+        if (password == "--")
         {
-          mylcd.clear();
-          mylcd.setCursor(1-1, 1-1);
-          mylcd.print("Casa Inteligenta");
-          mylcd.setCursor(1-1, 2-1);
-          mylcd.print("Pass");
-          mylcd.setCursor(11-1, 2-1);
-          mylcd.print("OK");
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Smart Home");
+          lcd.setCursor(0, 1);
+          lcd.print("Pass");
+          lcd.setCursor(10, 1);
+          lcd.print("OK");
           servo_9.write(180);
           delay(0);
-          usa = 0;
-          acces_usa = true;          
+          door = 0;
+          doorAccess = true;
         }
         else
         {
-          mylcd.clear();
-          mylcd.setCursor(1-1, 1-1);
-          mylcd.print("Casa Inteligenta");
-          mylcd.setCursor(1-1, 2-1);
-          mylcd.print("Pass:");
-          mylcd.setCursor(11-1, 2-1);
-          mylcd.print("Err");
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Smart Home");
+          lcd.setCursor(0, 1);
+          lcd.print("Pass:");
+          lcd.setCursor(10, 1);
+          lcd.print("Err");
           tone(3, 165);
           delay(125);
           delay(500);
           noTone(3);
           delay(200);
-          mylcd.clear();
-          mylcd.setCursor(1-1, 1-1);
-          mylcd.print("Casa Inteligenta");
-          mylcd.setCursor(11-1, 2-1);
-          mylcd.print("Iar");
-          mylcd.setCursor(1-1, 2-1);
-          mylcd.print("Parola:");
-          cheie();
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Smart Home");
+          lcd.setCursor(10, 1);
+          lcd.print("Again");
+          lcd.setCursor(0, 1);
+          lcd.print("Password:");
+          play_key_tone();
         }
       }
-   
-       else if (true == acces_usa)
-       {
-         acces_usa = false;
-         mylcd.clear();
-         mylcd.setCursor(1-1, 1-1);
-         mylcd.print("Casa Inteligenta");
-         servo_9.write(110);
-         delay(0);
-       }
-    parola = "";
+      else // doorAccess == true
+      {
+        doorAccess = false;
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Smart Home");
+        servo_9.write(110);
+        delay(0);
+      }
+      password = "";
+    }
   }
-}
 }
  
 void setup()
@@ -136,47 +137,47 @@ void setup()
   slave.begin(SLAVE_ID, 9600, SERIAL_8N1);
   slave.configureHoldingRegisters(registers, 8);
  
-// initializare variabile partea 1  
+  // initialize variables part 1  
   pinMode(A1, INPUT);
   pinMode(A4, INPUT);
   pinMode(7, OUTPUT);
  
-// initializare variabile partea 2
-  pinMode (A0, INPUT);
+  // initialize variables part 2
+  pinMode(A0, INPUT);
   servo_11.attach(11);
  
-// initializare variabile partea 3
+  // initialize variables part 3
   pinMode(A2, INPUT);
   servo_10.attach(10);
  
-// initializare variabile partea 4
+  // initialize variables part 4
   pinMode(13, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(A3, INPUT);
  
-// initializare variabile partea 5
+  // initialize variables part 5
   pinMode(8, INPUT);
   pinMode(6, OUTPUT);
   digitalWrite(6, LOW);
  
-// initializare variabile partea 6
+  // initialize variables part 6
   dht12.begin();
   pinMode(6, OUTPUT);
   digitalWrite(6, LOW);
  
-// initializare variabile partea 7
+  // initialize variables part 7
   pinMode(3, OUTPUT);
-  rosu = 1;
-  verde = 1;
-  timp = 0;
-  parola = "";
-  usa = 0;
-  acces_usa = false;
-  mylcd.init();
-  mylcd.backlight();
+  red = 1;
+  green = 1;
+  timeCounter = 0;
+  password = "";
+  door = 0;
+  doorAccess = false;
+  lcd.init();
+  lcd.backlight();
   servo_9.attach(9);
-  mylcd.setCursor(1-1, 1-1);
-  mylcd.print("Casa Inteligenta");
+  lcd.setCursor(0, 0);
+  lcd.print("Smart Home");
   servo_9.write(110);
   pinMode(5, INPUT);
   pinMode(4, INPUT);
@@ -184,128 +185,126 @@ void setup()
  
 void loop()
 {
-  digitalWrite(DE_RE, LOW);  
+  digitalWrite(DE_RE, LOW);
  
   if (slave.poll()) {
-digitalWrite(DE_RE, HIGH);
-delay(10);
-digitalWrite(DE_RE, LOW);
+    digitalWrite(DE_RE, HIGH);
+    delay(10);
+    digitalWrite(DE_RE, LOW);
   }
  
-// program principal partea 1
-    registers[0] = analogRead(A1);
-    registers[1] = analogRead(A4);
-    if (registers[0] > 500)
+  // main program part 1
+  registers[0] = analogRead(A1);
+  registers[1] = analogRead(A4);
+  if (registers[0] > 500)
+  {
+    if (registers[1] == 1)
     {
-      if (registers[1] == 1)
-      {
-        digitalWrite(7,HIGH); // porneste LED-ul rosu
-        delay(6000);
-        digitalWrite(7,LOW); // taie tensiunea de alimentare a LED-ului
-      }
+      digitalWrite(7, HIGH); // turn on red LED
+      delay(6000);
+      digitalWrite(7, LOW);  // turn off red LED
     }
-    else
-    {
-      digitalWrite(7,LOW);
-    }
+  }
+  else
+  {
+    digitalWrite(7, LOW);
+  }
  
-// program principal partea 2
-    registers[2] = digitalRead(A0); // moneda introdusa afiseaza 0
-    if (registers[2] == 0)
-    {
-      servo_11.write(180);
-    }
-    else if (registers[2] == 1)
-    {
-      servo_11.write(90);
-    }
+  // main program part 2
+  registers[2] = digitalRead(A0); // coin inserted reads 0
+  if (registers[2] == 0)
+  {
+    servo_11.write(180);
+  }
+  else if (registers[2] == 1)
+  {
+    servo_11.write(90);
+  }
  
-// program principal partea 3
-    registers[3] = analogRead(A2);
-    if (100 < registers[3])
-    {
-      servo_10.write(90); // inchide fereastra
-    }
-    else
-    {
-      servo_10.write(0); // pastreaza deschisa fereastra
-    }
+  // main program part 3
+  registers[3] = analogRead(A2);
+  if (registers[3] > 100)
+  {
+    servo_10.write(90); // close window
+  }
+  else
+  {
+    servo_10.write(0);  // keep window open
+  }
  
-// program principal partea 4
-    registers[4] = analogRead(A3);
-    if (50 > registers[4])
-    {
-      alarma_sol();
-    }
-    else
-    {
-      digitalWrite(13,LOW);
-      noTone(3);
-    }
+  // main program part 4
+  registers[4] = analogRead(A3);
+  if (registers[4] < 50)
+  {
+    sound_alarm();
+  }
+  else
+  {
+    digitalWrite(13, LOW);
+    noTone(3);
+  }
  
-// program principal partea 5
-    registers[5] = digitalRead(8);
-    if (1 == registers[5]) // daca senzorul sesizeaza radiatie luminoasa
-    {
-      ventilator (6, 120); // ventilatorul functioneaza pe pinul digital 6 la viteza 120/255
-    }
-    else
-    {
-      ventilator (6, 0);
-    }
+  // main program part 5
+  registers[5] = digitalRead(8);
+  if (registers[5] == 1) // if light sensor detects radiation
+  {
+    control_fan(6, 120); // fan on pin 6 at speed 120/255
+  }
+  else
+  {
+    control_fan(6, 0);
+  }
  
-// program principal partea 6
-    registers[6] = dht12.readTemperature(); // grade Celsius
+  // main program part 6
+  registers[6] = dht12.readTemperature(); // degrees Celsius
+  registers[7] = dht12.readHumidity();    // percent
+  if (registers[6] > 30)
+  {
+    control_fan(6, 60);
+  }
+  else
+  {
+    control_fan(6, 0);
+  }
  
-    registers[7] = dht12.readHumidity(); // in %
-    if (30 < registers[6])
+  // main program part 7
+  green = digitalRead(4);
+  red = digitalRead(5);  // 1 if button not pressed, 0 if pressed
+  if (green != 0 && red == 0)
+  {
+    delay(100);
+    green = digitalRead(4);
+    while (green != 0 && red == 0)
     {
-      ventilator (6, 60);
-    }
-    else
-    {
-      ventilator (6, 0);
-    }
- 
-// program principal partea 7
-    verde = digitalRead(4);
-    rosu = digitalRead(5);  //1 daca butonul nu e apasat, 0 daca este apasat
-    if (0 != verde && 0 == rosu)
-    {
+      red = digitalRead(5);
+      timeCounter++;
       delay(100);
-      verde = digitalRead(4);
-      while (0 != verde && 0 == rosu)
-      {
-        rosu = digitalRead(5);
-        timp = timp + 1;
-        delay(100);
-      }
     }
-    if (1 < timp && 5 > timp)
-    {
-      cheie();
-      parola = String(parola) + String(".");
-      mylcd.clear();
-      mylcd.setCursor(1-1, 1-1);
-      mylcd.print("Casa Inteligenta");
-      mylcd.setCursor(1-1, 2-1);
-      mylcd.print("Parola:");
-      mylcd.setCursor(11-1, 2-1);
-      mylcd.print(parola);
-    }
-    if (5 < timp)
-    {
-      cheie();
-      parola = String(parola) + String("-");
-      mylcd.clear();
-      mylcd.setCursor(1-1, 1-1);
-      mylcd.print("Casa Inteligenta");
-      mylcd.setCursor(1-1, 2-1);
-      mylcd.print("Parola:");
-      mylcd.setCursor(11-1, 2-1);
-      mylcd.print(parola);
-    }
-  confirmare_parola();
-  timp = 0;
+  }
+  if (timeCounter > 1 && timeCounter < 5)
+  {
+    play_key_tone();
+    password += ".";
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Smart Home");
+    lcd.setCursor(0, 1);
+    lcd.print("Password:");
+    lcd.setCursor(10, 1);
+    lcd.print(password);
+  }
+  if (timeCounter > 5)
+  {
+    play_key_tone();
+    password += "-";
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Smart Home");
+    lcd.setCursor(0, 1);
+    lcd.print("Password:");
+    lcd.setCursor(10, 1);
+    lcd.print(password);
+  }
+  confirm_password();
+  timeCounter = 0;
 }
-
